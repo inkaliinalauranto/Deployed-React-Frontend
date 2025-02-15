9ef3f3914d798a9663715ee08359e9270ec8029f
Inka-Liina Lauranto <inka-liina.lauranto@edu.lapinamk.fi>
2025-02-15 13:09:14 +0200

    Tehty mobiiliresponsiivisen tunne-analyysin käyttöliittymä ulkoasullisesti valmiiksi.

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,67 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
+import positive from './assets/positive.svg'
+import negative from './assets/negative.svg'
+import neutral from './assets/neutral.svg'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [isDisabled, setIsDisabled] = useState(true)
+  const [isResult, setIsResult] = useState(false)
+  const [feeling, setFeeling] = useState("")
+  const [inputSentence, setInputSentence] = useState("")
+  const [icon, setIcon] = useState(neutral)
+
+  function getSentence(e: any) {
+    e.preventDefault()
+    setInputSentence("")
+    setIsDisabled(true)
+    setIsResult(true)
+    // Arvotaan lauseen tunnelma, koska backendia ei vielä ole:
+    const feelings = ["negative", "neutral", "positive"]
+    const icons = [negative, neutral, positive]
+
+    // Tallennetaan muuttujaan satunnainen numero väliltä 0-2: 
+    const randomNumber = Math.floor(Math.random() * 3)
+    // console.log(randomNumber)
+
+    // Asetetaan feelings-arraysta feeling-tilamuuttujaan arvotun numeron 
+    // määrittämän alkion arvo:
+    setFeeling(feelings[randomNumber])
+    setIcon(icons[randomNumber])
+
+    console.log(feelings[randomNumber])
+  }
+
+  useEffect(() => {
+    if (inputSentence.toString().trim().length !== 0) {
+      setIsDisabled(false)
+    } else {
+      setIsDisabled(true)
+    }
+  }, [inputSentence])
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {!isResult ?
+        <div>
+          <h6>Sentiment analysis</h6>
+          <h4>What are you currently thinking?</h4>
+          <form onSubmit={getSentence}>
+            <input placeholder='Write a word or a sentence here' type="text" value={inputSentence} onChange={(e) => setInputSentence(e.target.value)} />
+            <button disabled={isDisabled} type="submit">Send</button>
+          </form>
+        </div> :
+        <div>
+          <div className="card">
+            <p>
+              Your sentiment is {feeling}
+            </p>
+            <img src={icon} className="logo react" alt="Positive logo" />
+            <button onClick={() => { setIsResult(false) }}>Back</button>
+          </div>
+        </div>
+      }
     </>
   )
 }
